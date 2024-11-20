e81a0726f6e7d523ba98e8ac7598a9d7b3ea93ec
Mathias Roesler <mroe734@aucklanduni.ac.nz>
2024-11-20 15:30:33 +1300

    Reviewed formatting

--- include/AbstractUterineCellFactory3d.hpp
+++ include/AbstractUterineCellFactory3d.hpp
@@ -1,5 +1,5 @@
-#ifndef ABSTRACTUTERINECELLFACTORY3d_HPP
-#define ABSTRACTUTERINECELLFACTORY3d_HPP
+#ifndef INCLUDE_ABSTRACTUTERINECELLFACTORY3D_HPP_
+#define INCLUDE_ABSTRACTUTERINECELLFACTORY3D_HPP_
 
 #include <toml.hpp>
 #include "MonodomainProblem.hpp"
@@ -10,32 +10,27 @@
 #include "Tong2014Cvode.hpp"
 #include "Roesler2024Cvode.hpp"
 
-namespace USMC_3D_SYSTEM_CONSTANTS
-{
-	const std::string CONFIG_DIR = 
-		getenv("CHASTE_MODELLING_CONFIG_DIR");
-	const std::string GENERAL_PARAM_FILE = 
-		"3d_params.toml";
+namespace USMC_3D_SYSTEM_CONSTANTS {
+const std::string CONFIG_DIR = getenv("CHASTE_MODELLING_CONFIG_DIR");
+const std::string GENERAL_PARAM_FILE = "3d_params.toml";
 }
 
-class AbstractUterineCellFactory3d : public AbstractCardiacCellFactory<3>
-{
-protected:
-	std::string mpCell_type;
-	std::unordered_map<std::string, float> mpCell_parameters;
-	unsigned short int mpCell_id; // 0 = HH, 1 = CK, 2 = Means, 3 = Tong, 4 = Roesler
+class AbstractUterineCellFactory3d : public AbstractCardiacCellFactory<3> {
+ protected:
+  std::string mpCell_type;
+  std::unordered_map<std::string, float> mpCell_parameters;
+  std::int16_t mpCell_id;  // 0 = HH, 1 = CK, 2 = Means, 3 = Tong, 4 = Roesler
 
 
-public:
-	AbstractUterineCellFactory3d();
-	AbstractCvodeCell* CreateCardiacCellForTissueNode(Node<3>* pNode);
-	std::string GetCellType();
-	virtual void ReadParams(std::string general_param_file);
-	virtual void ReadCellParams(std::string cell_param_file);
-	virtual void PrintParams();
-	virtual void WriteLogInfo(std::string log_file);
-
+ public:
+  AbstractUterineCellFactory3d();
+  AbstractCvodeCell* CreateCardiacCellForTissueNode(Node<3>* pNode);
+  std::string GetCellType();
+  virtual void ReadParams(std::string general_param_file);
+  virtual void ReadCellParams(std::string cell_param_file);
+  virtual void PrintParams();
+  virtual void WriteLogInfo(std::string log_file);
 };
 
-#endif // ABSTRACTUTERINECELLFACTORY3d_HPP
+#endif  // INCLUDE_ABSTRACTUTERINECELLFACTORY3D_HPP_
 
