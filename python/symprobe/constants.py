bb8dcbce5cb04ecebd54b30721f12f192d94b999
Mathias Roesler <mroe734@aucklanduni.ac.nz>
2024-11-27 14:02:20 +1300

    Updated the constants

--- python/symprobe/constants.py
+++ python/symprobe/constants.py
@@ -13,31 +13,21 @@ import os
 # Define global constants
 HOME = os.path.expanduser("~")
 BASE = "Documents/phd"
-RESISTIVITY = 1e-2  # Intracellular resistivity
+RESISTIVITY = 1e-1  # Intracellular resistivity
 CONFIG_ENV_VAR = "CHASTE_MODELLING_CONFIG_DIR"
 PTS_LIST = {  # List of points to extract for the scaffolds
-    "uterus_scaffold_0": [195, 265, 329],
-    "uterus_scaffold_1": [365, 499, 627],
-    "uterus_scaffold_2": [725, 995, 1251],
-    "uterus_scaffold_3": [1088, 1493, 1877],
+    "uterus_scaffold_1": [725, 995, 1251],
+    "uterus_scaffold_2": [1401, 1927, 2439],
+    "uterus_scaffold_3": [1591, 2189, 2765],
     "uterus_scaffold_4": [1595, 2192, 2768],
-    "uterus_scaffold_5": [2387, 3284, 4148],
-    "uterus_scaffold_6": [3183, 4379, 5531],
-    "uterus_scaffold_7": [4195, 5775, 7311],
-    "uterus_scaffold_8": [5587, 7695, 9743],
-    "uterus_scaffold_9": [6984, 9619, 12179],
+    "uterus_scaffold_5": [2387, 3824, 4148],
 }
 DIST_DICT = {  # Average distance between elements for the scaffolds
-    "uterus_scaffold_0": 1.27,
-    "uterus_scaffold_1": 0.77,
-    "uterus_scaffold_2": 0.70,
-    "uterus_scaffold_3": 0.65,
+    "uterus_scaffold_1": 0.70,
+    "uterus_scaffold_2": 0.46,
+    "uterus_scaffold_3": 0.52,
     "uterus_scaffold_4": 0.48,
     "uterus_scaffold_5": 0.46,
-    "uterus_scaffold_6": 0.44,
-    "uterus_scaffold_7": 0.35,
-    "uterus_scaffold_8": 0.34,
-    "uterus_scaffold_9": 0.33,
 }
 CONVERSION_IDX = [  # List of node indices for each tetrahedra
     [0, 1, 2, 4],  # Tetrahedron 1
