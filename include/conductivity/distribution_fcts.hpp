96c8def10f7f3d8cbc57f97166e261b97ce01563
Shawn Means <s.means@auckland.ac.nz>
2025-07-14 04:28:54 +0100

    Added random noise to passive coupling

--- include/conductivity/distribution_fcts.hpp
+++ include/conductivity/distribution_fcts.hpp
@@ -3,14 +3,16 @@
 
 #include <iostream>
 #include <cmath>
+#include <random>
 
 #include "Exception.hpp"
 
 
 double linear_distribution(double z, double baseline, double slope,
-                           double centre);
+                           double centre, double mean, double stddev);
 double gaussian_distribution(double z, double baseline, double slope,
-                             double centre, double amplitude);
+                             double centre, double amplitude, double mean, 
+                             double stddev);
 
 #endif  // INCLUDE_CONDUCTIVITY_DISTRIBUTION_FCTS_HPP_
 
--- src/conductivity/distribution_fcts.cpp
+++ src/conductivity/distribution_fcts.cpp
@@ -1,8 +1,19 @@
 #include "../../include/conductivity/distribution_fcts.hpp"
 
 double linear_distribution(double z, double baseline, double slope,
-                           double centre) {
-  double value = baseline + (z - centre)*slope;
+                           double centre, double mean, double stddev) {
+
+  //apply normal distribution to the baseline value   
+  // Create a random number generator engine
+  std::mt19937 generator(std::random_device{}());        
+  // Define the normal distribution with mean 0 and standard deviation 1
+  std::normal_distribution<double> distribution(mean, stddev);
+  double mod_baseline = distribution(generator)*baseline;               
+
+  double value = mod_baseline + (z - centre)*slope;
+
+  //debug
+  //std::cout << "(distribution_fcts.hpp) baseline: " << baseline << " modified: " << mod_baseline << std::endl;
 
   if (value < 0.0) {
     return 0.0;
@@ -12,13 +23,30 @@ double linear_distribution(double z, double baseline, double slope,
 }
 
 double gaussian_distribution(double z, double baseline, double slope,
-                             double centre, double amplitude) {
+                             double centre, double amplitude, double mean, 
+                             double stddev) {
+
+  //apply normal distribution to the baseline value   
+  // Create a random number generator engine
+  std::mt19937 generator(std::random_device{}());        
+  // Define the normal distribution with mean 0 and standard deviation 1
+  std::normal_distribution<double> distribution(mean, stddev);
+  double mod_baseline = distribution(generator)*baseline;
+
   double value;
-  value = baseline + amplitude*std::exp(-slope*std::pow(z - centre, 2.0));
+  value = mod_baseline*amplitude*std::exp(-slope*std::pow(z - centre, 2.0));
+
+  //std::cout << "(distribution_fcts.hpp) baseline: " << baseline << " modified: " << mod_baseline << std::endl;
+  
 
   if (value < 0.0) {
+    //std::cout << "(distribution_fcts.hpp) z: " << z << " returned value: 0.0 " << std::endl;
     return 0.0;
   } else {
+    //std::cout << "(distribution_fcts.hpp) z: " << z << " returned value: " << value << std::endl;
     return value;
   }
+
+  
+
 }
